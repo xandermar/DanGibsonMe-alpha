2482d2747fb0391c4307c1f84f59796b6d60056a
xandermar <daniel.gibson@xandermar.com>
2025-06-05 10:35:19 -0400

    feat: integrate Google Analytics for improved tracking of page views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+// Declare gtag globally
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dangibson-me';
+
+  constructor(private router: Router) {
+    // Track page views on route change
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-EQ67EPWCH3', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
